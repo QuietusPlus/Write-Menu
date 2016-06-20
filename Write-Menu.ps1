d3b1bbd054cceb58407dd2fb3404d94ccec86d88
QuietusPlus <quietus.plus@gmail.com>
2016-06-20 20:14:15 +0200

    Change license to GNU GPLv3

--- Write-Menu.ps1
+++ Write-Menu.ps1
@@ -1,10 +1,26 @@
+<#
+    Write-Menu: Outputs a command-line menu which can be navigated using the keyboard.
+    Copyright (C) 2016 QuietusPlus
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
 function Write-Menu {
     <#
         .NOTES
-            Write-Menu (v1.0)
+            Write-Menu
             by QuietusPlus
-
-            LICENSE: This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
             Based on "Simple Textbased Powershell Menu" by Michael Albert [info@michlstechblog.info]
 
